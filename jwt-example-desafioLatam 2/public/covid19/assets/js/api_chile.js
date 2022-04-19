7ef4e3e535be57e08152d82cecefb4251e81fb70
Fabian <fabian@debian.desarrollo>
2022-04-18 23:01:45 -0400

    se documenta app

--- jwt-example-desafioLatam 2/public/covid19/assets/js/api_chile.js
+++ jwt-example-desafioLatam 2/public/covid19/assets/js/api_chile.js
@@ -2,119 +2,114 @@ import { menuChile } from "./menu.js";
 import Logout from "./logout.js";
 
 const situacionChile = async () => {
-	const token = localStorage.getItem("jwt-token");
-	if (token) {
-		menuChile(token);
-	}
-	Logout();
+  const token = localStorage.getItem("jwt-token");
+  if (token) {
+    menuChile(token);
+  }
+  Logout();
 };
+
+//funcion que realiza 3 llamados a apis distintas de tipo get
+//itera sus respuestas y las deja en un arrays vacios para luego pasarlos al grafico
 
 const datos = async () => {
-	const token = localStorage.getItem("jwt-token");
-	var confirmed = [];
-	var recover = [];
-	var deaths = [];
+  const token = localStorage.getItem("jwt-token");
+  var confirmed = [];
+  var recover = [];
+  var deaths = [];
 
-	try {
-		const response = await fetch(`http://localhost:3000/api/confirmed`, {
-			//tipo de request
-			method: "GET",
-			headers: {
-				//tipo de autorizacion mas el token para validar
-				Authorization: `Bearer ${token}`,
-			},
-		});
-		//trae la data en formato json
-		const { data } = await response.json();
+  try {
+    const response = await fetch(`http://localhost:3000/api/confirmed`, {
+      //tipo de request
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const { data } = await response.json();
 
-		data.forEach((element) => {
-			confirmed.push(element);
-		});
+    data.forEach((element) => {
+      confirmed.push(element);
+    });
 
-		const response1 = await fetch(`http://localhost:3000/api/recovered`, {
-			//tipo de request
-			method: "GET",
-			headers: {
-				//tipo de autorizacion mas el token para validar
-				Authorization: `Bearer ${token}`,
-			},
-		});
-		//trae la data en formato json
-		const { data: data1 } = await response1.json();
+    const response1 = await fetch(`http://localhost:3000/api/recovered`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const { data: data1 } = await response1.json();
 
-		data1.forEach((element) => {
-			recover.push(element);
-		});
+    data1.forEach((element) => {
+      recover.push(element);
+    });
 
-		const response2 = await fetch(`http://localhost:3000/api/deaths`, {
-			//tipo de request
-			method: "GET",
-			headers: {
-				//tipo de autorizacion mas el token para validar
-				Authorization: `Bearer ${token}`,
-			},
-		});
-		//trae la data en formato json
-		const { data: data2 } = await response2.json();
+    const response2 = await fetch(`http://localhost:3000/api/deaths`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const { data: data2 } = await response2.json();
 
-		data2.forEach((element) => {
-			deaths.push(element);
-		});
+    data2.forEach((element) => {
+      deaths.push(element);
+    });
 
-		grafico(confirmed, recover, deaths);
+    grafico(confirmed, recover, deaths);
 
-		if (data && data1 && data2) {
-			let elemento = document.getElementById("loader");
-			elemento.className += "d-none";
+    //condicional que esconde la animacion loader cuando se completa la carga de los datos
 
-			document.getElementById("fondo").className += "bg-dark";
-		}
-	} catch (err) {
-		// limpia el local store si el token no es valido
-		/* localStorage.removeItem("jwt-token")
-		console.error(`Error: ${err}`);*/
-	}
+    if (data && data1 && data2) {
+      let elemento = document.getElementById("loader");
+      elemento.className += "d-none";
+
+      document.getElementById("fondo").className += "bg-dark";
+    }
+  } catch (err) {}
 };
 
+//funcion que devuelve un grafico con los datos extraidos de la funcion anterior
+
 const grafico = (confirmed, recover, deaths) => {
-	let myChart;
-	const dato = {
-		labels: confirmed.slice(4, 800).map((item) => item.date),
-		datasets: [
-			{
-				label: "Confirmados",
-				backgroundColor: "rgb(255, 205, 86)",
-				data: confirmed.slice(4, 800).map((item) => item.total),
-				fill: false,
-				tension: 0.1,
-			},
-			{
-				label: "Recuperados",
-				backgroundColor: "rgb(75, 192, 192)",
-				data: recover.slice(4, 800).map((item) => item.total),
-				fill: false,
-				tension: 0.1,
-			},
-			{
-				label: "Muertes",
-				backgroundColor: "rgb(201, 203, 207)",
-				data: deaths.slice(4, 800).map((item) => item.total),
-				fill: false,
-				tension: 0.1,
-			},
-		],
-	};
+  let myChart;
+  const dato = {
+    labels: confirmed.slice(4, 800).map((item) => item.date),
+    datasets: [
+      {
+        label: "Confirmados",
+        backgroundColor: "rgb(255, 205, 86)",
+        data: confirmed.slice(4, 800).map((item) => item.total),
+        fill: false,
+        tension: 0.1,
+      },
+      {
+        label: "Recuperados",
+        backgroundColor: "rgb(75, 192, 192)",
+        data: recover.slice(4, 800).map((item) => item.total),
+        fill: false,
+        tension: 0.1,
+      },
+      {
+        label: "Muertes",
+        backgroundColor: "rgb(201, 203, 207)",
+        data: deaths.slice(4, 800).map((item) => item.total),
+        fill: false,
+        tension: 0.1,
+      },
+    ],
+  };
 
-	const config = {
-		type: "line",
-		data: dato,
-	};
+  const config = {
+    type: "line",
+    data: dato,
+  };
 
-	if (myChart instanceof Chart) {
-		myChart.destroy();
-	}
+  if (myChart instanceof Chart) {
+    myChart.destroy();
+  }
 
-	myChart = new Chart(document.getElementById("myChartChile"), config);
+  myChart = new Chart(document.getElementById("myChartChile"), config);
 };
 
 datos();
--- jwt-example-desafioLatam 2/public/covid19/assets/js/consult.js
+++ jwt-example-desafioLatam 2/public/covid19/assets/js/consult.js
@@ -1,12 +1,15 @@
+//consulta a la api que devuelve todos los datos para insertarlos en un grafico o tabla
+
 const consult = (() => {
   const url = "http://localhost:3000/api/total";
   const getData = async () => {
     const resp = await fetch(url);
     const data = await resp.json();
     return data;
-  }; {
+  };
+  {
     return {
-      getData
+      getData,
     };
   }
 })();
